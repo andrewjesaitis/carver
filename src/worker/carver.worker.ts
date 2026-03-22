70af4ed2b4fff543df41a3d44c730184a1bb0f7a
Andrew Jesaitis <andrew@andrewjesaitis.com>
2026-03-22 00:22:50 -0700

    feat: add typed web worker for seam carving

---
+++ src/worker/carver.worker.ts
@@ -0,0 +1,26 @@
+/// <reference lib="webworker" />
+
+import type { ResizeRequest, ResizeResponse, ResizeError } from '../types';
+import { resize } from '../algorithm/carver';
+
+self.onmessage = (event: MessageEvent<ResizeRequest>) => {
+  try {
+    const { buffer, width, height, derivative, targetWidth, targetHeight } = event.data;
+    const srcImageData = new ImageData(new Uint8ClampedArray(buffer), width, height);
+    const result = resize(srcImageData, derivative, targetWidth, targetHeight);
+
+    const response: ResizeResponse = {
+      type: 'RESIZE',
+      buffer: result.data.buffer,
+      width: result.width,
+      height: result.height,
+    };
+    (self as unknown as Worker).postMessage(response, [response.buffer]);
+  } catch (err) {
+    const error: ResizeError = {
+      type: 'RESIZE_ERROR',
+      message: err instanceof Error ? err.message : String(err),
+    };
+    self.postMessage(error);
+  }
+};
